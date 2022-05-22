16aa17a02d38018dcf24302f2d3340c740912434
Ralph Lazana <lazanaralph@gmail.com>
2022-05-23 06:41:08 +0800

    remove smtp account

--- JobPortal/settings.py
+++ JobPortal/settings.py
@@ -136,9 +136,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST =  'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='rlazana23@gmail.com' #enter gmail that will be use for sending email
-EMAIL_HOST_PASSWORD = 'Adecade1' #enter gmail password
+
